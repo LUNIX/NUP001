6f27de07cde3560b2f7d78a85731a68a280cb9be
nick_FBI <nick_fbi@mail.ru>
2016-05-10 22:15:41 +0300

    Первый коммит.

---
+++ Assets/Control Scripts/FPSInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Scripts/FPSInput")]
+public class FPSInput : MonoBehaviour {
+    private CharacterController _charController;
+    public float gravity = -9.8f;
+    public float speed = 6.0f;
+	// Use this for initialization
+	void Start () {
+        _charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        transform.Translate(deltaX*Time.deltaTime, 0, deltaZ*Time.deltaTime);
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);  //Convert local to global
+        _charController.Move(movement);
+	}
+}
---
+++ Assets/Control Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+    public float speed = 3.0f;  //Const speed rotate
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, speed, 0);      //Run command Rotate in every frame, Self space
+	}
+}
